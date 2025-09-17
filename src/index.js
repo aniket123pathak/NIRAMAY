d306eca2a422a9c20d713c7b1eec32c587d3f62b
Aniket Pathak <aniketpathak581@gmail.com>
2025-09-17 20:34:29 +0530

    Add cookie-parser and cors middleware; update package.json and package-lock.json

--- src/index.js
+++ src/index.js
@@ -5,4 +5,14 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path : "./env"
 })
-connectDB();
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running On the Port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection Failed !!!",error);
+})
