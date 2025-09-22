8f1cb4e1610f2fe31a0949ba9142fddd54f9589d
Aniket Pathak <aniketpathak581@gmail.com>
2025-09-22 19:32:19 +0530

    Define Patient model schema with fields for personal details, medical history, and relationships to Doctor model

--- src/models/patient.model.js
+++ src/models/patient.model.js
@@ -1,5 +1,57 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const patientSchema = new Schema({}, {timeStamps : true});
+const patientSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      index: true,
+      lowercase: true,
+      required: true,
+      trim: true
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNo: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    weight: {
+      type: Number,
+    },
+    symptoms: {
+      type: [String],
+      lowercase: true,
+    },
+    medicalHistory: {
+      type: [String],
+    },
+    assignedDoctor: {
+        type: Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true
+    },
+    reports: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Patient = mongoose.model("Patient", patientSchema);
