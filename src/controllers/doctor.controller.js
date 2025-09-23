5c82822c9cefd7b7b6b6890a477f93d215992e9d
Aniket Pathak <aniketpathak581@gmail.com>
2025-09-24 00:47:36 +0530

    Enhance Doctor registration endpoint with validation and error handling

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -1,11 +1,84 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Doctor } from "../models/doctor.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const registerDoctor = asyncHandler ( async (req,res)=>{
-    res.status(200).json({
-        message : "Ok"
-    })
-})
+const registerDoctor = asyncHandler(async (req, res) => {
+  const {
+    userName,
+    email,
+    phoneNo,
+    password,
+    fullName,
+    qualifications,
+    specializations,
+  } = req.body;
+  if (
+    [userName, fullName, email, phoneNo, password].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new apiError(400, "All Fields are Required !!");
+  }
 
-export {
-    registerDoctor,
-}
+  if (!specializations || !qualifications) {
+    throw new apiError(400, "Add Specialozation or qualification");
+  }
+
+  const existedDoctor = await Doctor.findOne({
+    $or: [{ userName }, { email }, { phoneNo }],
+  });
+
+  if (!/^\d{10}$/.test(phoneNo)) {
+    throw new ApiError(
+      400,
+      "Phone number must be exactly 10 digits and contain no letters or symbols."
+    );
+  }
+
+  if (existedDoctor) {
+    throw new apiError(
+      409,
+      "Doctor with the same userName or email already exists!!!"
+    );
+  }
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar file is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar) {
+    throw new apiError(400, "Avatar file is required");
+  }
+
+  const doctor = await Doctor.create({
+    userName: userName.toLowerCase(),
+    fullName,
+    phoneNo,
+    email,
+    password,
+    avatar: avatar.url,
+    specializations,
+    qualifications,
+  });
+
+  const createdDoctor = await Doctor.findById(doctor._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdDoctor) {
+    throw new apiError(
+      500,
+      "Something went wrong while registering the Doctor.."
+    );
+  }
+
+  return res
+    .status(201)
+    .json(
+      new apiResponse(201, createdDoctor, "Doctor registered successfully")
+    );
+});
+
+export { registerDoctor };
--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -1,9 +1,13 @@
 import {Router} from "express"
 import { registerDoctor } from "../controllers/doctor.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router ();
 
-router.route("/register").post(registerDoctor)
+router.route("/register").post(
+    upload.single('avatar'),
+    registerDoctor
+)
 
 export default router
 
