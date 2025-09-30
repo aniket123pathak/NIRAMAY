708d6b4c651ce37e83fb1e1bbd28f784b0b44132
Aniket Pathak <aniketpathak581@gmail.com>
2025-09-30 11:53:35 +0530

    res

--- src/controllers/visit.controllers.js
+++ src/controllers/visit.controllers.js
@@ -6,45 +6,49 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const createVisit = asyncHandler(async (req,res)=>{
-    const { patientId , symptoms , dateOfVisit , medicine , bill} = req.body
+const createVisit = asyncHandler(async (req, res) => {
+  const { patientId, symptoms, dateOfVisit, medicine, bill } = req.body;
 
-    if(
-        [patientId , symptoms , dateOfVisit , medicine].some((field)=>field?.trim()==="")
-    ){
-        throw new apiError(400,"Every field is required")
-    }
+  if (
+    [patientId, symptoms, dateOfVisit, medicine].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new apiError(400, "Every field is required");
+  }
 
-    const patient = await Patient.findById(patientId);
+  const patient = await Patient.findById(patientId);
 
-    if(!patient){
-        throw new apiError(400,"Patient does not Exist")
-    }
+  if (!patient) {
+    throw new apiError(400, "Patient does not Exist");
+  }
 
-    const visit = await Visit.create({
-        patientId,
-        symptoms,
-        dateOfVisit,
-        medicine,
-        bill,
-    })
+  const visit = await Visit.create({
+    patientId,
+    symptoms,
+    dateOfVisit,
+    medicine,
+    bill,
+  });
 
-    if(!visit){
-        throw new apiError(500,"Something went wrong while creating the visit..!!!!")
-    }
+  if (!visit) {
+    throw new apiError(
+      500,
+      "Something went wrong while creating the visit..!!!!"
+    );
+  }
 
-    const createdVisit = await Visit.findById(visit._id)
+  const createdVisit = await Visit.findById(visit._id);
 
-    await Patient.findByIdAndUpdate(
-        patientId,
-        {
-            $push : {
-                visits :  createdVisit._id
-            }
-        }
-    ) 
-})
+  await Patient.findByIdAndUpdate(patientId, {
+    $push: {
+      visits: createdVisit._id,
+    },
+  });
 
-export {
-    createVisit
-}
+  return res
+    .status(201)
+    .jsos(new apiResponse(201, createdVisit, "Visit added Successfully"));
+});
+
+export { createVisit };
